93f3db994d970c9cc08b74024f9a7fd28ddbe130
Aleksey Pohodnya <pohodnyaa@gmail.com>
2015-08-10 22:39:31 +0600

    test send mail

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -5,6 +5,6 @@ ActionMailer::Base.smtp_settings = {
     port:                  587,
     domain:               'aeromontazh.herokuapp.com',
     user_name:            'aeromontazh@gmail.com',
-    password:             'nqbbtjsntvmbndvc',
+    password:             'fowldrismpvvtobu',
     authentication:       'plain',
     enable_starttls_auto:  true  }
