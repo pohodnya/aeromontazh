30eed94a814329c34716d708a536b05f81b5e4b2
Aleksey Pohodnya <pohodnyaa@gmail.com>
2015-08-13 14:43:48 +0600

    google auth

--- app/mailers/client_mailer.rb
+++ app/mailers/client_mailer.rb
@@ -1,6 +1,7 @@
 class ClientMailer < ActionMailer::Base
 
   def send_to_admin(client)
-    mail to: 'info@aero-montazh.ru', subject: 'Подтверждение регистрации', body: client, content_type: "text/html"
+    mail to: 'info@aero-montazh.ru', subject: 'Заявка с лэндинга', body: 'Имя: ' + client['name'] + '<br>Телефон: ' +
+                                       client['phone'] + '<br>Время: ' + DateTime.now.strftime('%d/%m/%Y %H:%M:%S'), content_type: "text/html"
   end
 end
