0d69afd5fd9e172a5d94e9220176386056035d6a
Aleksey Pohodnya <pohodnyaa@gmail.com>
2015-08-29 19:22:29 +0600

    Release

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,30 +25,7 @@ set :rbenv_roles, :all
 # А это рекомендуют добавить для приложений, использующих ActiveRecord
 set :puma_init_active_record, true
 
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
-
-  before :start, :make_dirs
-end
-
 namespace :deploy do
-  desc "Make sure local git is in sync with remote."
-  task :check_revision do
-    on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
-        puts "Run `git push` to sync changes."
-        exit
-      end
-    end
-  end
-
   desc 'Initial Deploy'
   task :initial do
     on roles(:app) do
@@ -56,16 +33,4 @@ namespace :deploy do
       invoke 'deploy'
     end
   end
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
-    end
-  end
-
-  before :starting,     :check_revision
-  after  :finishing,    :compile_assets
-  after  :finishing,    :cleanup
-  after  :finishing,    :restart
 end
