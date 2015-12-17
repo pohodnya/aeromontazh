4a62cb708c8d53b1b880ed8bc173eb59921fc801
Aleksey Pohodnya <pohodnyaa@gmail.com>
2015-12-17 16:30:50 +0600

    Release

--- app/assets/javascripts/scrollup.js
+++ app/assets/javascripts/scrollup.js
@@ -63,7 +63,7 @@ function sendMainOrder() {
 
 $(function () {
     var note = $('#note'),
-        ts = new Date(2015, 11, 15),
+        ts = new Date(2015, 11, 31),
         newYear = true;
 
     if((new Date()) > ts){
